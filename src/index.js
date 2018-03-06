bca349cef0f668c530fc1aeb936b7f03d80d312b
Anton Mikhailovskiy <anton.mikhailovskiy.87@gmail.com>
2018-03-07 02:04:07 +0300

    Longest consecutive sequence Solution

--- src/index.js
+++ src/index.js
@@ -4,44 +4,25 @@ module.exports = function longestConsecutiveLength(array) {
         return 0;
     }
 
-    var set = [];
-    var longestConsecutiveLength = 1;
+    array.sort((a, b) => a - b);
 
-    while (true) {
-        // console.log("init array = " + array);
-        // console.log("arrayLength start = " + array.length);
-        var left = array[array.length - 1] - 1;
-        var right = array[array.length - 1] + 1;
-        var count = 1;
+    var countMax = 1;
+    var count = 1;
+    var diff = 0;
 
-        // console.log(left, right);
-
-        while (array.indexOf(left) != -1) {
-            count++;
-            // console.log("array element = " + array[array.indexOf(left)]);
-            array.splice(array.indexOf(left), 1);
-            left--;
-            // console.log("array after decrement = " + array);
+    for (var i = 0; i < array.length - 1; i++) {
+        diff = array[i + 1] - array[i];
+        if (diff == 0) {
+            continue;
         }
-
-        while (array.indexOf(right) != -1) {
+        if (diff == 1) {
             count++;
-            // console.log("array element = " + array[array.indexOf(right)]);
-            array.splice(array.indexOf(right), 1);
-            right++;
-            // console.log("array after increment = " + array);
+            if (countMax < count) {
+                countMax = count;
+            }
+        } else {
+            count = 1;
         }
-
-        // console.log("array before pop = " + array);
-        array.pop();
-        // console.log("array after pop = " + array);
-
-        longestConsecutiveLength = Math.max(count, longestConsecutiveLength);
-
-        // console.log("longestConsecutiveLength = " + longestConsecutiveLength);
-        // console.log("---------------------------");
-        if (array.length == 0)
-            break;
     }
-    return longestConsecutiveLength;
+    return countMax;
 }
